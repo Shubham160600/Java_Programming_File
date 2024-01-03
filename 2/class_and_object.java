a5236b97a646ba39696850095e9128dc64e44b95
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 15:24:42 +0530

    Create class_and_object.java

---
+++ 2/class_and_object.java
@@ -0,0 +1,31 @@
+/* 
+To create a Java program using classes and objects. */
+
+class Person {
+	// Instance Variables
+	String name;
+	int age;
+
+	// Constructor
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	// Method to display information about the person
+	public void displayInfo() {
+		System.out.println("Name:" + name);
+		System.out.println("Age:" + age);
+	}
+}
+
+public class class_and_object {
+	public static void main(String[] args) {
+
+		// Create an object of the Person class
+		Person person1 = new Person("Shubham", 22);
+
+		// Call the displayInfo() method on the object
+		person1.displayInfo();
+	}
+}
