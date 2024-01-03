38eaff7692b2b7c0244a67ed94ffaf77f6bc9f4d
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 23:22:17 +0530

    Create MisspelledVar.java

---
+++ 15/MisspelledVar.java
@@ -0,0 +1,10 @@
+package shubham;
+
+public class MisspelledVar {
+
+    public static void main(String[] args) {
+    	int a=40, b=60;
+    	int Sum=a+b;
+    	System.out.println("Sum of variables is " + Sum);
+    }
+}
